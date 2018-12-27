890b12678fd9890162f714ae4fd2f816fcddae56
Bruno Besson <bruno.besson@gmail.com>
2018-12-27 21:32:17 +0100

    LGTM + eslint fixes

--- tools/generate-docs.js
+++ tools/generate-docs.js
@@ -1,46 +1,44 @@
-const fs = require('fs');
+const fs = require('fs')
 
-function processReadme(prefix, parent, name, result){
+function processReadme(prefix, parent, name, result) {
     let data
 
     try {
         data = fs.readFileSync(parent + '/' + name + '/README.md', 'utf-8')
-        data = data.replace("\r", "").split("\n")[0]
-    }
-    catch(error) {
-        data = " "
+        data = data.replace(/\r/g, '').split('\n')[0]
+    } catch (error) {
+        data = ' '
     }
 
     result.push([prefix + name, data])
 }
 
-function walkSync(dir, result, prefix=""){
+function walkSync(dir, result, prefix = '') {
+    for (let subDir of fs.readdirSync(dir)) {
+        let path = dir + '/' + subDir
 
-    for(let subDir of fs.readdirSync(dir)){
-        let path = dir + "/" + subDir
-
-        if(fs.statSync(path).isDirectory()) {
+        if (fs.statSync(path).isDirectory()) {
             processReadme(prefix, dir, subDir, result)
-            walkSync(path, result, prefix + "    ");
+            walkSync(path, result, prefix + '    ')
         }
     }
 }
 
 function main() {
     var result = []
-    walkSync("src", result)
+    walkSync('src', result)
 
-    data = ["| **directory** | **comment** |\n|---|---|"]
-    for(let line of result){
-        data.push(`|${line[0].replace(/ /g, "&nbsp;")}|${line[1]}|`)
+    const data = ['| **directory** | **comment** |\n|---|---|']
+    for (let line of result) {
+        data.push(`|${line[0].replace(/ /g, '&nbsp;')}|${line[1]}|`)
     }
 
-    fs.writeFileSync("docs/source-structure.md", data.join("\n"))
+    fs.writeFileSync('docs/source-structure.md', data.join('\n'))
 }
 
-    // If running this module directly then call the main function.
+// If running this module directly then call the main function.
 if (require.main === module) {
-    main();
+    main()
 }
 
-module.exports = main;
+module.exports = main
