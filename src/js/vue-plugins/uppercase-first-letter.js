a9cc54825009053e380792b8cba32714335581e5
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2019-01-10 10:56:42 +0100

    uppercaseFirstLetter filter, as CSS trick does not works on all browsers

---
+++ src/js/vue-plugins/uppercase-first-letter.js
@@ -0,0 +1,8 @@
+// CSS trick for this does not work on all browsers...
+// use a vue filter to perform This
+
+export default function install(Vue) {
+    Vue.filter('uppercaseFirstLetter', function(value) {
+        return value.charAt(0).toUpperCase() + value.slice(1)
+    })
+}
