b308bcc7cba0dd87546e18bb243adcbd59f0c7ca
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2019-01-10 20:39:42 +0100

    Add GPS marker in pretty outing link

--- src/components/generics/pretty-links/PrettyOutingLink.vue
+++ src/components/generics/pretty-links/PrettyOutingLink.vue
@@ -9,9 +9,10 @@
         <router-link :to="{name:'profile', params:{id:outing.author.user_id}}">
             {{ outing.author.name }}
         </router-link>
-        <span v-if="outing.img_count!=0">
+        <span v-if="outing.img_count!=0 || outing.geometry.has_geom_detail">
             &hairsp;&bull;&hairsp;
-            <marker-image-count :image-count="outing.img_count"/>
+            <marker-image-count v-if="outing.img_count!=0" :image-count="outing.img_count"/>
+            <marker-gps-trace v-if="outing.geometry.has_geom_detail"/>
         </span>
     </span>
 </template>
