33ee4e147a8fbab1e77ea25d4dd4f6417bd88c5b
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2019-02-13 15:53:18 +0100

    good size for query item labels

--- src/views/documents/utils/QueryItemSliderLabel.vue
+++ src/views/documents/utils/QueryItemSliderLabel.vue
@@ -1,25 +1,25 @@
 <template>
     <div>
-        <div class="level is-mobile"> <!--level must be in a single element to remove bottim margin : TODO remove ugly hack -->
-            <span class="level-left">
-                <span class="level-item query-bound-value">
-                    <span v-if="field.i18n">{{ $gettext(value[0], field.i18nContext) }}</span>
-                    <span v-else>{{ value[0] }}</span>
-                    <span v-if="field.unit">&nbsp;{{ field.unit }}</span>
-                </span>
-            </span>
-            <span class="level-item">
-                <span class=" query-label">
-                    {{ $gettext(field.name) | uppercaseFirstLetter }}
-                </span>
-            </span>
-            <span class="level-right">
-                <span class="level-item query-bound-value">
-                    <span v-if="field.i18n">{{ $gettext(value[1], field.i18nContext) }}</span>
-                    <span v-else>{{ value[1] }}</span>
-                    <span v-if="field.unit">&nbsp;{{ field.unit }}</span>
-                </span>
-            </span>
+        <div class="columns is-mobile"> <!--level must be in a single element to remove bottim margin : TODO remove ugly hack -->
+            <div class="column query-bound-value-left">
+                <span v-if="field.i18n">{{ $gettext(value[0], field.i18nContext) | uppercaseFirstLetter }}</span>
+                <span v-else>{{ value[0] }}</span>
+                <span v-if="field.unit">&nbsp;{{ field.unit }}</span>
+            </div>
+            <!-- class depends on i18n ???
+        * if values are text (like awful, good... for conditions), we can't know which one will need some place
+          ==> every item as same place : 1 third of all place
+        * but of values are numeric (like elevation), then we know that there textual representation is small
+          ==> give to field's name all the place it needs with is-narrow helper
+      -->
+            <div class="column query-label" :class="{'is-narrow':!field.i18n, 'is-4':field.i18n}">
+                {{ $gettext(field.name) | uppercaseFirstLetter }}
+            </div>
+            <div class="column query-bound-value-right">
+                <span v-if="field.i18n">{{ $gettext(value[1], field.i18nContext) | uppercaseFirstLetter }}</span>
+                <span v-else>{{ value[1] }}</span>
+                <span v-if="field.unit">&nbsp;{{ field.unit }}</span>
+            </div>
         </div >
     </div>
 
@@ -45,10 +45,15 @@
 
 .query-label{
     font-style:italic;
+    text-align:center;
 }
 
-.query-bound-value{
+.query-bound-value-left, .query-bound-value-right{
     font-weight:bold;
+}
+
+.query-bound-value-right{
+    text-align:right;
 }
 
 </style>
