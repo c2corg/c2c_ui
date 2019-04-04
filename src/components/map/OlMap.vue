3382abea198a32fd734fd03962551718cf3fb0df
Bruno Besson <bruno.besson@gmail.com>
2019-04-04 23:00:20 +0200

    Add context in effective code

--- src/components/map/OlMap.vue
+++ src/components/map/OlMap.vue
@@ -17,14 +17,14 @@
               <header v-translate>Base layer</header>
               <div v-for="layer of mapLayers" :key="layer.title" @click="visibleLayer=layer">
                 <input :checked="layer==visibleLayer" type="radio">
-                {{ $gettext(layer.get('title')) }}
+                {{ $gettext(layer.get('title', 'Map layer')) }}
               </div>
             </td>
             <td>
               <header v-translate>Slopes</header>
               <div v-for="layer of dataLayers" :key="layer.title" @click="toogleMapLayer(layer)">
                 <input :checked="layer.getVisible()" type="checkbox">
-                {{ $gettext(layer.get('title')) }}
+                {{ $gettext(layer.get('title'), 'Map slopes layer') }}
               </div>
             </td>
             <td v-if="showProtectionAreas && !editable">
