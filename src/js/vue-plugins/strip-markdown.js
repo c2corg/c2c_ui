64a772c0d0f0dcb18eb1c0a59b982c7710a5e491
Bruno Besson <bruno.besson@gmail.com>
2019-04-04 20:37:59 +0200

    Do note remove dash unless surrounded by white space or line return

--- src/js/vue-plugins/strip-markdown.js
+++ src/js/vue-plugins/strip-markdown.js
@@ -5,6 +5,7 @@ export default function install(Vue) {
     return value
       .replace(/\[([^\]]+)\]\([^)]+\)/g, '$1') // external links
       .replace(/\[\[[^|]+|([^\]]+)\]\]/g, '$1') // internal links
-      .replace(/[*_\-#~`]/g, ''); // emphasis, lists and other similar stuff
+      .replace(/([\\s\n])-(\\s\n)/g, '$1$2') // dash, but only when preceded and followed by a space chare or line return
+      .replace(/[*_\\#~`]/g, ''); // emphasis and other similar stuff
   });
 }
