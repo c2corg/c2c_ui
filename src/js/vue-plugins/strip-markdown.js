46fa63dd25478777647cc7f535716e5b6e3e1cd8
Bruno Besson <bruno.besson@gmail.com>
2019-04-17 14:04:25 +0200

    Fix typo for internal links

--- src/js/vue-plugins/strip-markdown.js
+++ src/js/vue-plugins/strip-markdown.js
@@ -4,7 +4,7 @@ export default function install(Vue) {
   Vue.filter('stripMarkdown', function(value) {
     return value
       .replace(/\[([^\]]+)\]\([^)]+\)/g, '$1') // external links
-      .replace(/\[\[[^|]+|([^\]]+)\]\]/g, '$1') // internal links
+      .replace(/\[\[[^|]+\|([^\]]+)\]\]/g, '$1') // internal links
       .replace(/([\\s\n])-(\\s\n)/g, '$1$2') // dash, but only when preceded and followed by a space chare or line return
       .replace(/[*_\\#~`]/g, ''); // emphasis and other similar stuff
   });
