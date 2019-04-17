430c3152f6223b431d55424eb42872759feec0ff
Bruno Besson <bruno.besson@gmail.com>
2019-04-17 14:05:38 +0200

    Strip sub and sup

--- src/js/vue-plugins/strip-markdown.js
+++ src/js/vue-plugins/strip-markdown.js
@@ -6,6 +6,7 @@ export default function install(Vue) {
       .replace(/\[([^\]]+)\]\([^)]+\)/g, '$1') // external links
       .replace(/\[\[[^|]+\|([^\]]+)\]\]/g, '$1') // internal links
       .replace(/([\\s\n])-(\\s\n)/g, '$1$2') // dash, but only when preceded and followed by a space chare or line return
-      .replace(/[*_\\#~`]/g, ''); // emphasis and other similar stuff
+      .replace(/[*_\\#~`]/g, '') // emphasis and other similar stuff
+      .replace(/<su[bp]>(.+)<\/su[bp]>/g, '$1'); // sup
   });
 }
