de6c179027f56cf679ea02a785e05fdeef280d11
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2019-05-11 18:27:07 +0200

    Add a plugin that makes screen dimension responsive : handle responviness by script

--- src/main.js
+++ src/main.js
@@ -16,6 +16,7 @@ import globalComponents from '@/js/vue-plugins/generic-components';
 import helperWindow from '@/js/vue-plugins/helper-window';
 import imageViewer from '@/js/vue-plugins/image-viewer';
 import localStorage from '@/js/vue-plugins/local-storage';
+import screen from '@/js/vue-plugins/screen';
 import stripMarkdown from '@/js/vue-plugins/strip-markdown';
 import upperCaseFirstLetter from '@/js/vue-plugins/uppercase-first-letter';
 import user from '@/js/vue-plugins/user';
@@ -68,6 +69,7 @@ Vue.use(helperWindow); // vm.$helper property
 Vue.use(alertWindow); // vm.$alert property
 Vue.use(imageViewer);
 Vue.use(globalComponents); // Components available everywhere
+Vue.use(screen); // screen reactives properties
 Vue.use(stripMarkdown); // stripMarkdown filter
 Vue.use(upperCaseFirstLetter); // upperCaseFirstLetter filter
 Vue.use(user); // vm.$user property
