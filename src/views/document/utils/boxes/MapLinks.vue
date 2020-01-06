f16c6dfec70786fc2dc2cd83f17be70852d0efb3
cbeauchesne <charles.de.beauchesne@gmail.com>
2020-01-06 22:15:26 +0100

    Reduce map links visibility #844

---
+++ src/views/document/utils/boxes/MapLinks.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="associations-list">
+    <div v-if="document.maps_info">
+      <icon-map />
+      <span>
+        {{ document.maps_info }}
+      </span>
+    </div>
+    <div v-for="map of document.maps" :key="map.document_id" class="is-ellipsed">
+      <icon-map />
+      <document-link :document="map">
+        {{ map.editor }} - {{ map.code }} - <document-title :document="map" />
+      </document-link>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { requireDocumentProperty } from '@/js/properties-mixins';
+
+  export default {
+    mixins: [ requireDocumentProperty ]
+  };
+</script>
