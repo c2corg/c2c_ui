fecb88e03238e93c4cad62f7578aca8bf389edd1
Bruno Besson <bruno.besson@gmail.com>
2020-05-09 15:31:08 +0200

    use actions to create release description

--- scripts/update-release-description.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -e
-
-firstTag=$(git tag | sort -V -r | head -1)
-secondTag=$(git tag | sort -V -r | head -2 | awk '{split($0, tags, "\n")} END {print tags[1]}')
-git log --pretty=oneline ${secondTag}..${firstTag} --no-merges | node scripts/update-release-description.js
