4b1925ed414aeb04141c48dd5e3c88c491870304
AntoineM <antoine.marnat@gmail.com>
2020-12-08 15:24:38 +0100

    Update get-fundraiser.js
    un comment and sectors for Presles

--- src/js/get-fundraiser.js
+++ src/js/get-fundraiser.js
@@ -1,50 +1,102 @@
 const mapping = {
-  // 14274: { // France FFME
+  // France
+  // 14274: { // FFME - Rockclimber
   //   url: ' https://www.helloasso.com/associations/rockclimber/formulaires/1/widget'
   // },
   
   //Greenspit
-  // 947587: { //Mollans
-  // url: 'https://greenspits.com/topo/mollans-sur-ouveze/'
-  // },
-  // 1150854: { //Ramirole
-  // url: 'https://greenspits.com/topo/la-ramirole-2/'
-  // },
-  // 820728: { //Maupas
-  // url: 'https://greenspits.com/topo/la-carriere-du-maupas/'
-  // },
+  947587: { //Mollans
+    url: 'https://greenspits.com/topo/mollans-sur-ouveze/'
+  },
+  1150854: { //Ramirole
+    url: 'https://greenspits.com/topo/la-ramirole-2/'
+  },
+  820728: { //Maupas
+    url: 'https://greenspits.com/topo/la-carriere-du-maupas/'
+  },
   
-  // 14080: { // Access Fund USA
-  // url: 'https://www.accessfund.org/join-or-give'
-  // },
-
-  // UK Boltfund
-  // 1203403: { //Peak District
-  // url : 'http://thepeakboltfund.blogspot.com/'
-  // },
-  // 1203390: { //Lake District
-  // url : 'https://www.cumbriaboltfund.co.uk/bolting'
-  // },
-  // 1264698: { //Cheshire
-  // url: 'https://www.paypal.com/donate/?cmd=_s-xclick&hosted_button_id=XQ4S2XU3WH5F6'
-  // },
-  
-  107049: { //VTNO
+  // VTNO : Site et secteurs de Presles
+  107049: { //Presles
+    url: 'https://www.vtno.fr/adh%C3%A9sion/',
+  },
+  318456: { //Presles - Dalarhum
+    url: 'https://www.vtno.fr/adh%C3%A9sion/',
+  },
+  276273 : { //Presles - École d'artif
+    url: 'https://www.vtno.fr/adh%C3%A9sion/',
+  },
+  177942: { //Presles - Triangle de Choranche
+    url: 'https://www.vtno.fr/adh%C3%A9sion/',
+  },
+  176860: { //Presles - Daladom
+    url: 'https://www.vtno.fr/adh%C3%A9sion/',
+  },
+  103555: { //Presles - Rocher de Nugues, Tina dalle
+    url: 'https://www.vtno.fr/adh%C3%A9sion/',
+  },
+  103083: { //Presles - Balme étrange
+    url: 'https://www.vtno.fr/adh%C3%A9sion/',
+  },
+  103082: { //Presles - Pierrot beach
+    url: 'https://www.vtno.fr/adh%C3%A9sion/',
+  },
+  43267: { // Presles - Rocher des Nugues
+    url: 'https://www.vtno.fr/adh%C3%A9sion/',
+  },
+  42469: { // Presles - Dalles de Télébus
+    url: 'https://www.vtno.fr/adh%C3%A9sion/',
+  },
+  42426: { //Presles - Pas du Ranc
+    url: 'https://www.vtno.fr/adh%C3%A9sion/',
+  },
+  42425: { //Presles - Grottes de Choranche
+    url: 'https://www.vtno.fr/adh%C3%A9sion/',
+  },
+  42253: { //Presles - Paroi Rouge
+    url: 'https://www.vtno.fr/adh%C3%A9sion/',
+  },
+  41298: { //Presles - Fond du cirque
+    url: 'https://www.vtno.fr/adh%C3%A9sion/',
+  },
+  40768: { //Presles - Chrysanthèmes
+    url: 'https://www.vtno.fr/adh%C3%A9sion/',
+  },
+  40767: { //Presles - Fhara Kiri
+    url: 'https://www.vtno.fr/adh%C3%A9sion/',
+  },
+  40766: { //Presles - Eliane
+    url: 'https://www.vtno.fr/adh%C3%A9sion/',
+  },
+  40756: { //Presles - Buis
     url: 'https://www.vtno.fr/adh%C3%A9sion/',
   },
 
-  14474: {
-    // Scotland
+  // America
+  14080: { // Access Fund USA
+    url: 'https://www.accessfund.org/join-or-give'
+  },
+
+  // UK Boltfund
+  1203403: { //Peak District
+    url : 'http://thepeakboltfund.blogspot.com/'
+  },
+  1203390: { //Lake District
+    url : 'https://www.cumbriaboltfund.co.uk/bolting'
+  },
+  1264698: { //Cheshire
+    url: 'https://www.paypal.com/donate/?cmd=_s-xclick&hosted_button_id=XQ4S2XU3WH5F6'
+  },
+  
+  14474: { // Scotland
     url: 'https://www.7amax.co.uk/',
   },
 
-  14151: {
-    // Malta
+  //Others
+  14151: { // Malta
     url: 'https://maltaclimbingclub.org/bolt-fund/',
   },
 
-  14125: {
-    // Philippines
+  14125: { // Philippines
     url: 'http://www.climbphilippines.com/boltfund.html',
   },
 };
