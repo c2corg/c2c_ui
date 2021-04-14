d91c71de4db07215ade7a84a407438f87d83eedc
Bruno Besson <bruno.besson@non.se.com>
2021-04-14 15:31:51 +0200

    WTF? Fix ES2020 plugins use
    Don't know why we have to explicitely add them, they should be in babel-preset-env...

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,5 @@
 module.exports = {
   presets: ['@vue/cli-plugin-babel/preset'],
+  // don't know why we have to include these ES2020 plugins now, should be in babel-preset-env...
+  plugins: ['@babel/plugin-proposal-nullish-coalescing-operator', '@babel/plugin-proposal-optional-chaining'],
 };
