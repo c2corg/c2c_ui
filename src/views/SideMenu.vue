a9ee9b427c2d40618b5b4fee7c95a81c7d7fcd90
AntoineMarnat <antoine.marnat@gmail.com>
2021-06-01 18:28:54 +0200

    First try, problem with icon finding

--- src/views/SideMenu.vue
+++ src/views/SideMenu.vue
@@ -79,12 +79,12 @@
 
       <div class="columns is-gapless has-text-centered is-mobile menu-socials">
         <div class="column">
-          <a href="https://twitter.com/camptocamporg" title="twitter">
-            <fa-icon :icon="['fab', 'twitter']" class="twitter-icon" />
+          <a href="https://www.instagram.com/camptocamp_org/" title="Instagram">
+            <fa-icon :icon="['fab', 'twitter']" class="instagram-icon" />
           </a>
         </div>
         <div class="column">
-          <a href="https://www.facebook.com/camptocamp.org/" title="facebook">
+          <a href="https://www.facebook.com/camptocamp.org/" title="Facebook">
             <fa-icon :icon="['fab', 'facebook']" class="facebook-icon" />
           </a>
         </div>
@@ -207,15 +207,15 @@ aside {
     margin-right: calc((200px - 160px) / 2);
   }
 
-  .twitter-icon,
+  .instagram-icon,
   .facebook-icon,
   .donate-icon {
     font-size: 30px;
   }
 
-  .twitter-icon,
+  .instagram-icon,
   &:hover {
-    color: #4198fb; // twitter color
+    color: #e1306c; // instagram color
   }
 
   .facebook-icon,
