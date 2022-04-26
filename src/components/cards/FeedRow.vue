ca52e0fd4a60f4b5e6f42caebba1e2e5701e83bd
flob38 <florence.berbey@gmail.com>
2022-04-26 14:23:56 +0200

    Add document link, cosmetic

--- src/components/cards/FeedRow.vue
+++ src/components/cards/FeedRow.vue
@@ -1,68 +1,39 @@
 <template>
-  <div :document="document" class="card" :class="{ 'is-highlighted': highlighted }">
-    <span :title="$gettext('User avatar')">
-      <img
-        v-if="!useDefaultAvatarIcon"
-        class="avatar"
-        alt="Avatar"
-        @error="useDefaultAvatarIcon = true"
-        :src="$options.forumAvatarUrl + item.user.forum_username + '/36/1_1.png'"
-        loading="lazy"
-      />
-      <fa-icon v-else icon="user" class="is-size-1 has-text-grey" />
-    </span>
-    <marker-document-type :document-type="documentType" class="is-pulled-left is-size-1" />
-    <document-title :document="item.document" class="has-text-weight-bold" />
-    <span v-if="documentType === 'outing'" class="is-nowrap has-left-margin-mobile">{{ dates }}</span>
-
-    <!---
-    <card-row v-if="images.length">
-      <gallery :images="images" />
-    </card-row>
-
-    <card-row v-if="documentType != 'article' && documentType != 'book'">
-      <span v-if="documentType === 'outing' || documentType === 'route'">
-        <icon-ratings class="card-icon" />
-        <document-rating :document="item.document" />
+  <div class="card" :class="{ 'is-highlighted': highlighted }">
+    <document-link :document="document" :target="target">
+      <span :title="$gettext('User avatar')">
+        <img
+          v-if="!useDefaultAvatarIcon"
+          class="avatar"
+          alt="Avatar"
+          @error="useDefaultAvatarIcon = true"
+          :src="$options.forumAvatarUrl + item.user.forum_username + '/36/1_1.png'"
+          loading="lazy"
+        />
+        <fa-icon v-else icon="user" class="is-size-3 has-text-grey" />
       </span>
+      <marker-document-type :document-type="documentType" class="is-pulled-left is-size-3" />
 
-      <card-elevation-item :elevation="item.document.elevation_max" class="is-ellipsed" />
+      <document-title :document="item.document" class="row has-text-weight-bold is-ellipsed" />
 
-      <span v-if="item.document.height_diff_up" :title="$gettext('height_diff_up')">
-        <icon-height-diff-up />
-        {{ item.document.height_diff_up }}&nbsp;m
+      <span v-if="documentType === 'outing'">{{ dates }}</span>
+      <!-- <span v-if="item.document.areas && item.document.areas.length" class="row is-ellipsed" >
+        <card-region-item :document="item.document" />
+      </span> -->
+
+      <span class="is-pulled-right">
+        <span>
+          <card-activities-item v-if="item.document.activities" :activities="item.document.activities" />
+        </span>
+        <span>
+          <marker-soft-mobility v-if="documentType === 'outing' && item.document.public_transport" />
+          <marker-image-count :image-count="item.document.img_count" />
+          <marker-gps-trace v-if="item.document.geometry && item.document.geometry.has_geom_detail" />
+          <marker-condition v-if="documentType === 'outing'" :condition="item.document.condition_rating" />
+          <marker-quality :quality="item.document.quality" />
+        </span>
       </span>
-
-      <span v-if="item.document.height_diff_difficulties" :title="$gettext('height_diff_difficulties')">
-        <fa-icon icon="arrows-alt-v" />
-        {{ item.document.height_diff_difficulties }}&nbsp;m
-      </span>
-
-      <card-elevation-item v-if="item.document.elevation" :elevation="item.document.elevation" />
-
-    </card-row>
--->
-
-    <span v-if="item.document.areas && item.document.areas.length">
-      <card-region-item :document="item.document" />
-    </span>
-
-    <span>
-      <span>
-        <card-activities-item v-if="item.document.activities" :activities="item.document.activities" />
-      </span>
-      <span>
-        <marker-soft-mobility v-if="documentType === 'outing' && item.document.public_transport" />
-        &nbsp;
-        <marker-image-count :image-count="item.document.img_count" />
-        &nbsp;
-        <marker-gps-trace v-if="item.document.geometry && item.document.geometry.has_geom_detail" />
-      </span>
-      <span>
-        <marker-condition v-if="documentType === 'outing'" :condition="item.document.condition_rating" />
-        <marker-quality :quality="item.document.quality" />
-      </span>
-    </span>
+    </document-link>
   </div>
 </template>
 
@@ -88,6 +59,10 @@ export default {
     highlighted: {
       type: Boolean,
       default: false,
+    },
+    target: {
+      type: String,
+      default: undefined,
     },
   },
 
@@ -157,56 +132,13 @@ export default {
 </script>
 
 <style scoped lang="scss">
-/*@media screen and (max-width: $tablet) {
- .feed-card {
-    border-left: 0 !important;
-    border-right: 0 !important;
-  }
-  .has-left-margin-mobile {
-    margin-left: 5px;
-  }
-}*/
-
-.is-max-3-lines-height {
-  // proprietary stuff, supported on limited browsers
-  display: -webkit-box;
-  -webkit-box-orient: vertical;
-  -webkit-line-clamp: 3;
-  overflow: hidden;
-  text-overflow: ellipsis;
-}
-
-.card-image-content {
-  display: flex;
-  overflow: hidden;
-}
-
 .avatar {
   border-radius: 50%;
-  width: 36px;
-  height: 36px;
+  width: 24px;
+  height: 24px;
   vertical-align: bottom;
   margin-right: 0.5rem;
   margin-left: 0.5rem;
-}
-
-.has-3-images > div {
-  width: 33.33%;
-  max-height: 275px;
-  overflow: hidden;
-}
-
-.has-2-images > div {
-  width: 50%;
-}
-
-.has-2-images > div {
-  width: 100%;
-}
-
-.card-image-content > div > img {
-  width: 100%;
-  box-sizing: border-box;
 }
 
 .card {
@@ -215,10 +147,20 @@ export default {
   background: white;
 }
 
+a {
+  color: #363636 !important;
+}
+
 .card:hover,
 .is-highlighted {
   transition: 0.2s;
   box-shadow: $card-hover-shadow;
   background: $hover-background;
+}
+
+.row {
+  display: inline-block;
+  max-height: 2em;
+  max-width: 75%;
 }
 </style>
