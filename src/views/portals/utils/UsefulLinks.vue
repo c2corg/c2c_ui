874c9ac28a33b7ae818a2d4a777d145fc3cc3009
flob38 <florence.berbey@gmail.com>
2022-08-26 01:10:24 +0200

    Work on "display parameters" button

---
+++ src/views/portals/utils/UsefulLinks.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="box">
+    <h4 class="title is-3">Liens utiles</h4>
+    <div>
+        <ul>
+        <li>
+            <router-link :to="{ name: 'article', params: { id: 107228 } }" v-translate>Préparer sa course</router-link>
+        </li>
+        <li>
+            <router-link :to="{ name: 'yeti' }" v-translate>Yeti : un outil pour évaluer le risque d’avalanche</router-link>
+        </li>
+        <li>
+            <a href="https://www.metaskirando.ovh/" title="Metaskirando">Metaskirando : sorties de ski de rando en Europe</a>
+        </li>
+        <li>
+            <router-link :to="{ name: 'serac' }" v-translate>SERAC : accidents et incidents en montagne</router-link>
+        </li>
+        </ul>
+        <ul>
+        <li>
+            <router-link :to="{ name: 'article', params: { id: 106726 } }" v-translate>Camptocamp Association</router-link>
+        </li>
+        <li>
+            <a href="https://www.helloasso.com/associations/camptocamp-association/" title="Helloasso">Faire un don ou adhérer</a>
+        </li>
+        </ul>
+    </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+ul {
+  list-style-type: disc !important;
+  padding-left: 12px;
+}
+</style>
