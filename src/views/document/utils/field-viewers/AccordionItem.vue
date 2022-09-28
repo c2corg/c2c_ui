e6923a737f754184de449f6fc3e0184ca6725133
JP <jphilippe.ds@gmail.com>
2022-09-28 11:26:21 +0200

    Create accordion item component

---
+++ src/views/document/utils/field-viewers/AccordionItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="accordion-title" :class="{ 'no-print': !visible }">
+      <slot name="title" />
+      <fa-icon
+        class="is-size-6 has-cursor-pointer no-print accordion-icon"
+        icon="angle-down"
+        :rotation="visible ? undefined : 180"
+        @click="visible = !visible"
+      />
+    </div>
+    <div v-show="visible">
+      <slot name="content" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      visible: true,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.accordion-title {
+  position: relative;
+}
+
+.accordion-icon {
+  position: absolute;
+  top: 0;
+  right: 0;
+}
+</style>
