62c278fa432b051c9c2039368554ce7edef889d4
Bruno Besson <bruno.besson@gmail.com>
2023-01-29 15:46:09 +0100

    add missing API messages

--- src/views/user/utils/FormField.vue
+++ src/views/user/utils/FormField.vue
@@ -4,20 +4,24 @@
       {{ label }}
       <span v-if="required">*</span>
 
-      <!-- $gettext('Required', 'API message') -->
+      <!-- $gettext('already used username', 'API message') -->
+      <!-- $gettext('Invalid email address', 'API message') -->
+      <!-- $gettext('already used email', 'API message') -->
+      <!-- $gettext('Shorter than minimum length 3', 'API message') -->
+      <!-- $gettext('Longer than maximum length 25', 'API message') -->
+      <!-- $gettext('Contain invalid character(s)', 'API message') -->
+      <!-- $gettext('First character is invalid', 'API message') -->
+      <!-- $gettext('Last character is invalid', 'API message') -->
+      <!-- $gettext('Contains consecutive special characters', 'API message') -->
+      <!-- $gettext('Ended by confusing suffix', 'API message') -->
+      <!-- $gettext('already used forum_username', 'API message') -->
       <!-- $gettext('Password too short', 'API message') -->
       <!-- $gettext('Missing captcha', 'API message') -->
-      <!-- $gettext('already used username', 'API message') -->
-      <!-- $gettext('already used email', 'API message') -->
-      <!-- $gettext('Invalid email address', 'API message') -->
-      <!-- $gettext('Shorter than minimum length 3', 'API message') -->
-      <!-- $gettext('already used forum_username', 'API message') -->
+      <!-- $gettext('Required', 'API message') -->
       <!-- $gettext('Login failed', 'API message') -->
       <!-- $gettext('No user with this email', 'API message') -->
-
+      <!-- $gettext('Username cannot be empty or whitespaces', 'API message') -->
       <!-- $gettext('please consult the server logs', 'API message') -->
-
-      <!-- $gettext('Contain invalid character(s)', 'API message') -->
 
       <span class="has-text-danger has-text-weight-bold" v-if="errorMessage">
         {{ $gettext(errorMessage, 'API message') }}
