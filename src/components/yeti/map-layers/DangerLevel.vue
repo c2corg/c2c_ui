4d71b598d52980849a2713dc8a0944bdc9bae123
Vincent De Oliveira <vincent@iamvdo.me>
2023-02-10 10:47:14 +0100

    New dangerLevel's type when locLow/locHigh

--- src/components/yeti/map-layers/DangerLevel.vue
+++ src/components/yeti/map-layers/DangerLevel.vue
@@ -2,32 +2,49 @@
   <div>
     <svg :viewBox="danger.altitude ? '0 0 165 100' : '0 0 100 100'" :width="danger.altitude ? 130 : 80" height="80">
       <polygon points="2.2,89.5 97.5,89.5 62.7,11.9 48,32.9 31.8,25.5" />
-      <text x="50" :y="danger.high ? 82 : 65">
-        <tspan>{{ danger.low }}</tspan>
-        <tspan v-if="danger.lowEvol">&ensp;</tspan>
-        <tspan v-if="danger.lowEvol">{{ danger.lowEvol }}</tspan>
-      </text>
-      <g v-if="danger.lowEvol">
-        <line x1="45" x2="55" :y1="danger.high ? 75 : 58" :y2="danger.high ? 75 : 58" />
-        <line x1="50" x2="55" :y1="danger.high ? 70 : 53" :y2="danger.high ? 75 : 58" />
-        <line x1="50" x2="55" :y1="danger.high ? 80 : 63" :y2="danger.high ? 75 : 58" />
-      </g>
-      <g v-show="danger.high">
-        <line x1="10" y1="61.5" x2="90" y2="61.5" />
-        <text x="50" y="54">
-          <tspan>{{ danger.high }}</tspan>
-          <tspan v-if="danger.highEvol">&ensp;</tspan>
-          <tspan v-if="danger.highEvol">{{ danger.highEvol }}</tspan>
+      <g v-if="danger.locLow && !danger.altitude">
+        <text :x="locLowIsEast ? 80 : 5" y="40" class="small">
+          {{ danger.locLow }}
         </text>
-        <g v-if="danger.highEvol">
-          <line x1="45" x2="55" y1="46" y2="46" />
-          <line x1="50" x2="55" y1="41" y2="46" />
-          <line x1="50" x2="55" y1="51" y2="46" />
-        </g>
+        <text :x="!locLowIsEast ? 80 : 5" y="40" class="small">
+          {{ danger.locHigh }}
+        </text>
+        <text :x="locLowIsEast ? 66 : 33" y="68">
+          {{ danger.low }}
+        </text>
+        <line x1="50" x2="50" y1="50" y2="70" />
+        <text :x="!locLowIsEast ? 66 : 33" y="68">
+          {{ danger.high }}
+        </text>
       </g>
-      <text v-if="danger.altitude" x="92" y="65" class="small">
-        {{ danger.altitude }}
-      </text>
+      <g v-else>
+        <text x="50" :y="danger.high ? 82 : 65">
+          <tspan>{{ danger.low }}</tspan>
+          <tspan v-if="danger.lowEvol">&ensp;</tspan>
+          <tspan v-if="danger.lowEvol">{{ danger.lowEvol }}</tspan>
+        </text>
+        <g v-if="danger.lowEvol">
+          <line x1="45" x2="55" :y1="danger.high ? 75 : 58" :y2="danger.high ? 75 : 58" />
+          <line x1="50" x2="55" :y1="danger.high ? 70 : 53" :y2="danger.high ? 75 : 58" />
+          <line x1="50" x2="55" :y1="danger.high ? 80 : 63" :y2="danger.high ? 75 : 58" />
+        </g>
+        <g v-show="danger.high">
+          <line x1="10" y1="61.5" x2="90" y2="61.5" />
+          <text x="50" y="54">
+            <tspan>{{ danger.high }}</tspan>
+            <tspan v-if="danger.highEvol">&ensp;</tspan>
+            <tspan v-if="danger.highEvol">{{ danger.highEvol }}</tspan>
+          </text>
+          <g v-if="danger.highEvol">
+            <line x1="45" x2="55" y1="46" y2="46" />
+            <line x1="50" x2="55" y1="41" y2="46" />
+            <line x1="50" x2="55" y1="51" y2="46" />
+          </g>
+        </g>
+        <text v-if="danger.altitude" x="92" y="65" class="small">
+          {{ danger.altitude }}
+        </text>
+      </g>
     </svg>
   </div>
 </template>
