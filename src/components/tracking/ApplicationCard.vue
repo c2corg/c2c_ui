86ad197ab788f2d4a3e2170dd475d73669cc221e
Bruno Besson <bruno.besson@viseo.com>
2023-03-09 10:16:49 +0100

    darken not configured application cards

--- src/components/tracking/ApplicationCard.vue
+++ src/components/tracking/ApplicationCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card">
+  <div class="card" :class="{ 'has-background-light': application.status === 'not-configured' }">
     <div class="card-content">
       <div class="media">
         <div class="media-left">
