c2corg/c2c_ui

commit 13d082d178898eb4849ed23f9dc24650bd3dee4c

Better outing's edition form

src/views/wiki/edition/utils/MapInputRow.vue
@@ -23,19 +23,22 @@
       <div class="column is-12 map-container">
         <map-view
           ref="map"
+          :initial-extent="initialExtent"
+          :documents="documents"
           :edited-document="document"
           :geom-detail-editable="geomDetailEditable"
           show-center-on-geolocation
           show-recenter-on
+          @move="$emit('move', arguments[0])"
         />
       </div>
-      <div class="column is-6">
+      <div class="column is-6 is-hidden">
         <div class="field">
           <label class="label">{{ $gettext('Longitude') }}</label>
           <input-simple type="number" postfix="°E" v-model="longitude" @input="setGeometryPoint" />
         </div>
       </div>
-      <div class="column is-6">
+      <div class="column is-6 is-hidden">
         <div class="field">
           <label class="label">{{ $gettext('Latitude') }}</label>
           <input-simple type="number" postfix="°N" v-model="latitude" @input="setGeometryPoint" />
@@ -60,6 +63,14 @@ export default {
     geomDetailEditable: {
       type: Boolean,
       default: false,
+    },
+    documents: {
+      type: Array,
+      default: null,
+    },
+    initialExtent: {
+      type: Array,
+      default: null,
     },
   },
 
@@ -132,6 +143,10 @@ export default {
 
       this.longitude = Math.round(coords[0] * 1000000) / 1000000;
       this.latitude = Math.round(coords[1] * 1000000) / 1000000;
+    },
+
+    fitMapToDocuments() {
+      return this.$refs.map.fitMapToDocuments(true);
     },
   },
 };

src/components/generics/inputs/InputCheckbox.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="control">
+  <span class="input-checkbox">
     <input
       :id="'c2c-checkbox' + _uid"
       class="is-checkradio is-primary"
@@ -10,7 +10,7 @@
     <label :for="'c2c-checkbox' + _uid">
       <slot />
     </label>
-  </div>
+  </span>
 </template>
 
 <script>
@@ -29,9 +29,9 @@ export default {
 </script>
 
 <style scoped>
-.control {
+.input-checkbox {
   /* extension radio has removed padding around inputs */
-  padding-top: calc(0.375em - 1px);
+  display: inline-block;
   padding-bottom: calc(0.375em - 1px);
 }
 </style>

src/components/map/OlMap.vue
@@ -120,7 +120,7 @@ import { FIT } from '@/js/fit/FIT';
 import ol from '@/js/libs/ol';
 
 const DEFAULT_EXTENT = [-400000, 5200000, 1200000, 6000000];
-const DEFAULT_POINT_ZOOM = 12;
+const DEFAULT_POINT_ZOOM = 14;
 const MAX_ZOOM = 19;
 const TRACKING_INITIAL_ZOOM = 13;
 
@@ -188,6 +188,11 @@ export default {
       type: Object,
       default: null,
     },
+
+    initialExtent: {
+      type: Array,
+      default: null,
+    },
   },
 
   data() {
@@ -217,6 +222,11 @@ export default {
 
       // layer for document icons and paths
       documentsLayer: new ol.layer.Vector({
+        source: new ol.source.Vector(),
+      }),
+
+      // layer edited document
+      editionLayer: new ol.layer.Vector({
         source: new ol.source.Vector(),
       }),
 
@@ -290,29 +300,20 @@ export default {
     documents: {
       handler() {
         this.drawDocumentMarkers();
-        this.fitMapToDocuments();
       },
     },
 
-    editedDocument: {
-      handler() {
-        this.drawDocumentMarkers();
-        this.fitMapToDocuments();
+    'editedDocument.condition_rating': 'drawDocumentMarkers',
+    'editedDocument.waypoint_type': 'drawDocumentMarkers',
 
-        // when route is set on outing edition, geom may be updated
-        this.setDrawInteraction();
-      },
-      deep: true, // must look on change inside documents object
-    },
-
-    // if, for any reason, geomtry of edited document is set, center map on it
+    // if, for any reason, geometry of edited document is set, center map on it
     // * new value entered in text inputs
     // * first click
     // * new association to route (geometry is copied from route to outing in this case)
     'editedDocument.geometry.geom': {
       handler(to, from) {
         if (from === null && to !== null) {
-          this.fitMapToDocuments(true);
+          this.setDrawInteraction(); // reset interaction mode
         }
       },
     },
@@ -360,6 +361,7 @@ export default {
         this.imagesLayer, // images icons will be under documents
         this.documentsLayer,
         this.waypointsLayer, // keep waypoint above trace and documents
+        this.editionLayer,
       ],
 
       view: new ol.View({
@@ -376,6 +378,8 @@ export default {
 
     this.map.on('moveend', this.sendBoundsToUrl);
     this.map.on('moveend', this.getProtectionAreas);
+    this.map.on('moveend', this.emitMoveEvent);
+
     if (this.protectionAreasVisible) {
       this.protectionAreasLayers.forEach((layer) => layer.setVisible(true));
     }
@@ -394,6 +398,8 @@ export default {
 
     if (this.urlValue) {
       this.view.fit(this.urlValue, { size: this.map.getSize() });
+    } else if (this.initialExtent) {
+      this.fit(this.initialExtent);
     } else {
       this.fitMapToDocuments(true);
     }
@@ -414,7 +420,7 @@ export default {
 
   methods: {
     setModifyInteractions() {
-      const source = this.documentsLayer.getSource();
+      const source = this.editionLayer.getSource();
       const modify = new ol.interaction.Modify({ source });
 
       this.map.addInteraction(modify);
@@ -422,13 +428,13 @@ export default {
 
       modify.on('modifyend', (event) => {
         for (const feature of event.features.getArray()) {
-          this.updateDocumentGeometryFromFeature(feature);
+          this.setDocumentGeometry(feature.get('geometry'));
         }
       });
     },
 
     setDrawInteraction() {
-      const source = this.documentsLayer.getSource();
+      const source = this.editionLayer.getSource();
 
       if (this.drawInteraction) {
         this.map.removeInteraction(this.drawInteraction);
@@ -440,10 +446,14 @@ export default {
       this.editedDocument.geometry = this.editedDocument.geometry ?? {};
 
       if (!this.editedDocument.geometry.geom) {
-        this.drawInteraction = new ol.interaction.Draw({
-          source,
-          type: 'Point',
-        });
+        // the point is not set
+        if (this.editedDocument.type !== 'o') {
+          // do not let user set the point for outings
+          this.drawInteraction = new ol.interaction.Draw({
+            source,
+            type: 'Point',
+          });
+        }
       } else if (this.geomDetailEditable && !this.editedDocument.geometry.geom_detail) {
         this.drawInteraction = new ol.interaction.Draw({
           source,
@@ -471,21 +481,10 @@ export default {
         'addfeatures',
         function (event) {
           event.features.map(this.setDocumentGeometryFromFeature);
-          this.fitMapToDocuments(true);
         }.bind(this)
       );
 
       this.map.addInteraction(dragAndDrop);
-    },
-
-    updateDocumentGeometryFromFeature(feature) {
-      const document = feature.get('document');
-
-      if (!document) {
-        return;
-      }
-
-      this.setDocumentGeometry(document, feature.get('geometry'));
     },
 
     tryReadFeaturesFromGeoFile(file, format, options) {
@@ -501,7 +500,6 @@ export default {
         const features = this.tryReadFeaturesFromGeoFile(file, format, { featureProjection: 'EPSG:3857' });
         if (features?.length) {
           features.map(this.setDocumentGeometryFromFeature);
-          this.fitMapToDocuments(true);
           return;
         }
       }
@@ -513,7 +511,7 @@ export default {
 
     setDocumentGeometryFromFeature(feature) {
       const geometry = feature.get('geometry');
-      this.setDocumentGeometry(this.editedDocument, geometry);
+      this.setDocumentGeometry(geometry);
       this.drawDocumentMarkers();
       this.setDrawInteraction();
       this.setOutingStartDate(this.editedDocument, geometry);
@@ -544,13 +542,24 @@ export default {
       document.date_start = formatDate(new Date(timestamp * 1000), 'yyyy-MM-dd');
     },
 
-    setDocumentGeometry(document, geometry) {
+    setDocumentGeometry(geometry) {
+      const document = this.editedDocument;
       const geoJsonGeometry = geoJSONFormat.writeGeometryObject(geometry);
+
+      const change = (newGeometry) => {
+        if (newGeometry.geom) {
+          document.geometry.geom = newGeometry.geom;
+        }
+
+        if (newGeometry.geom_detail) {
+          document.geometry.geom_detail = newGeometry.geom_detail;
+        }
+      };
 
       if (geoJsonGeometry.type === 'Point') {
         // remove elevation and timestamp
         geoJsonGeometry.coordinates = geoJsonGeometry.coordinates.slice(0, 2);
-        document.geometry.geom = JSON.stringify(geoJsonGeometry);
+        change({ geom: JSON.stringify(geoJsonGeometry) });
       } else if (geoJsonGeometry.type === 'LineString' || geoJsonGeometry.type === 'MultiLineString') {
         if (document.type === 'a') {
           // areas need a polygon, let change the path, check that first and last point are the same
@@ -564,25 +573,27 @@ export default {
           }
         }
 
-        document.geometry.geom_detail = JSON.stringify(geoJsonGeometry);
+        change({ geom_detail: JSON.stringify(geoJsonGeometry) });
 
         if (!document.geometry.geom) {
           const mainLine = geometry.getType() === 'MultiLineString' ? geometry.getLineString(0) : geometry;
-          this.setDocumentGeometry(document, new ol.geom.Point(mainLine.getCoordinateAt(0.5)));
+          this.setDocumentGeometry(new ol.geom.Point(mainLine.getCoordinateAt(0.5)));
         }
       } else if (geoJsonGeometry.type === 'Polygon' || geoJsonGeometry.type === 'MultiPolygon') {
         // areas
-        document.geometry.geom_detail = JSON.stringify(geoJsonGeometry);
+        change({ geom_detail: JSON.stringify(geoJsonGeometry) });
       } else {
         throw new Error(`Unexpected geometry type : ${geometry.type}`);
       }
     },
 
     drawDocumentMarkers() {
+      const editionSource = this.editionLayer.getSource();
       const documentsSource = this.documentsLayer.getSource();
       const waypointsSource = this.waypointsLayer.getSource();
       const imagesSource = this.imagesLayer.getSource();
 
+      editionSource.clear();
       documentsSource.clear();
       waypointsSource.clear();
       imagesSource.clear();
@@ -590,7 +601,7 @@ export default {
       this.addDocumentFeature(this.oldDocument, documentsSource, buildDiffStyle(true));
       this.addDocumentFeature(this.newDocument, documentsSource, buildDiffStyle(false));
 
-      this.addDocumentFeature(this.editedDocument, documentsSource);
+      this.addDocumentFeature(this.editedDocument, editionSource);
 
       for (const document of this.documents ?? []) {
         this.addDocumentFeature(document, documentsSource);
@@ -711,6 +722,11 @@ export default {
       this.protectionAreasLayer.setVisible(!this.protectionAreasLayer.getVisible());
     },
 
+    emitMoveEvent() {
+      const bounds = this.view.calculateExtent();
+      this.$emit('move', bounds.map(Math.round));
+    },
+
     // If user want's to filter with map, it will send extent to url
     // otherwise, it set bbox url to undefined
     sendBoundsToUrl() {
@@ -723,14 +739,30 @@ export default {
       }
     },
 
+    fit(extent) {
+      this.view.fit(extent, { size: this.map.getSize() });
+
+      // slightly increase the extent, for more comfort
+      let zoom = this.view.getZoom() * 0.95;
+
+      // and set a minimum zoom level
+      zoom = Math.min(this.minZoomLevel, zoom);
+
+      this.view.setZoom(zoom);
+
+      return this.view.calculateExtent().map(Math.round);
+    },
+
     fitMapToDocuments(force) {
       if ((this.filterDocumentsWithMap || this.editable) && !force) {
-        return;
+        return this.view.calculateExtent().map(Math.round);
       }
+
+      this.drawDocumentMarkers();
 
       let extent = ol.extent.createEmpty();
 
-      for (const layer of [this.documentsLayer, this.waypointsLayer]) {
+      for (const layer of [this.editionLayer, this.documentsLayer, this.waypointsLayer]) {
         ol.extent.extend(extent, layer.getSource().getExtent());
       }
 
@@ -740,10 +772,7 @@ export default {
         extent = DEFAULT_EXTENT;
       }
 
-      this.view.fit(extent, { size: this.map.getSize() });
-
-      // set a minimum zoom level
-      this.view.setZoom(Math.min(this.minZoomLevel, this.view.getZoom()));
+      return this.fit(extent);
     },
 
     toogleMapLayer(layer) {

src/js/vue-plugins/document-utils.js
@@ -133,7 +133,7 @@ export default function install(Vue) {
         }
 
         array.push(child);
-        this.propagateAssociationProperties(document, child);
+        this.propagateProperties(document, child);
       },
 
       removeAssociation(document, child) {
@@ -143,16 +143,27 @@ export default function install(Vue) {
         document.associations[arrayName] = array.filter((item) => item.document_id !== child.document_id);
       },
 
-      propagateAssociationProperties(parent, child) {
-        // propagate route property to outing
-        if (parent.type !== 'o' || child.type !== 'r') {
-          return;
+      propagateProperties(parent, child) {
+        // Parent is being created/modified. When a child is associated to parent,
+        // it can be helpful to set parent's properties from child's properties.
+
+        if (parent.type === 'o' && child.type === 'r') {
+          this.propagateRoutePropertiesToOutingProperties(child, parent);
+        } else if (parent.type === 'r' && child.type === 'w') {
+          this.propagateGeomPoint(child, parent);
+        } else if (parent.type === 'x' && child.type === 'r') {
+          this.propagateGeomPoint(child, parent);
+        } else if (parent.type === 'x' && child.type === 'o') {
+          this.propagateGeomPoint(child, parent);
         }
+      },
 
-        const route = child;
-        const outing = parent;
+      propagateGeomPoint(from, to) {
+        to.geometry.geom = to.geometry.geom === null ? from.geometry.geom : to.geometry.geom;
+      },
 
-        outing.geometry.geom = outing.geometry.geom === null ? route.geometry.geom : outing.geometry.geom;
+      propagateRoutePropertiesToOutingProperties(route, outing) {
+        this.propagateGeomPoint(route, outing);
 
         const names = [
           'elevation_min',
@@ -329,6 +340,22 @@ export default function install(Vue) {
             return false;
           }
         }
+      },
+
+      getDocumentsBbox(documents) {
+        // given an array of documents, return the extent containing all these documents
+
+        let minX, maxX, minY, maxY;
+
+        for (const document of documents) {
+          const coordinates = JSON.parse(document.geometry.geom).coordinates;
+          maxX = maxX ? Math.max(maxX, coordinates[0]) : coordinates[0];
+          maxY = maxY ? Math.max(maxY, coordinates[1]) : coordinates[1];
+          minX = minX ? Math.min(minX, coordinates[0]) : coordinates[0];
+          minY = minY ? Math.min(minY, coordinates[1]) : coordinates[1];
+        }
+
+        return [minX, minY, maxX, maxY].map(Math.round);
       },
     },
   });

src/views/Advertisement.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="advertisement">
     <iframe
+      v-if="$options.isProduction"
       :title="$gettext('advertisement')"
       :src="$options.publicPath + 'affiz.html'"
       frameborder="0"
@@ -29,6 +30,7 @@ export default {
     };
   },
 
+  isProduction: config.isProduction,
   publicPath: config.publicPath,
 };
 </script>

src/views/document/utils/boxes/RecentOutingsBox.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="box no-print" v-if="!isDraftView && (documentType == 'route' || outings.length !== 0)">
+  <div class="box no-print" v-if="!isDraftView && (showAddOutingButton || outings.length !== 0)">
     <div class="title is-2">
       <span v-if="documentType === 'image'" v-translate>Associated outings</span>
       <span v-else v-translate>Last outings</span>
@@ -10,7 +10,7 @@
     </div>
 
     <div
-      v-if="(!hideSeeAllResultsButton && outings.length) || documentType == 'route'"
+      v-if="(!hideSeeAllResultsButton && outings.length) || showAddOutingButton"
       class="has-text-centered add-section"
     >
       <router-link
@@ -20,7 +20,7 @@
       >
         <span v-translate>show all</span>&nbsp;<span class="badge">{{ totalOutings }}</span>
       </router-link>
-      <add-link v-if="documentType == 'route'" document-type="outing" :query="query" class="button is-primary">
+      <add-link v-if="showAddOutingButton" document-type="outing" :query="query" class="button is-primary">
         <span v-translate v-if="outings.length === 0">Add the first outing</span>
       </add-link>
     </div>
@@ -55,8 +55,27 @@ export default {
 
     query() {
       const query = {};
-      query[this.document.type] = this.document.document_id;
+
+      if (this.documentType === 'waypoint') {
+        let bbox = this.$documentUtils.getDocumentsBbox(this.document.associations.all_routes.documents);
+        query.initial_bbox = bbox.join(',');
+      } else if (this.documentType === 'route') {
+        query[this.document.type] = this.document.document_id;
+      }
+
       return query;
+    },
+
+    showAddOutingButton() {
+      if (this.documentType === 'route') {
+        return true;
+      }
+
+      if (this.documentType === 'waypoint' && this.document.associations.all_routes.total !== 0) {
+        return true;
+      }
+
+      return false;
     },
   },
 };

src/views/documents/DocumentsView.vue
@@ -87,8 +87,7 @@
             :key="index"
             :class="{
               'is-full-mobile is-half-tablet is-half-desktop is-half-widescreen is-half-fullhd': showMap,
-              'is-full-mobile is-one-third-tablet is-one-third-desktop is-one-quarter-widescreen is-one-quarter-fullhd':
-                !showMap,
+              'is-full-mobile is-one-third-tablet is-one-third-desktop is-one-quarter-widescreen is-one-quarter-fullhd': !showMap,
             }"
             class="column card-container"
             @mouseenter="highlightedDocument = document"

src/views/user/AccountView.vue
@@ -37,9 +37,11 @@
       />
 
       <div class="field">
-        <input-checkbox v-model="is_profile_public">
-          <span v-translate>Make profile page public</span>
-        </input-checkbox>
+        <div class="control">
+          <input-checkbox v-model="is_profile_public">
+            <span v-translate>Make profile page public</span>
+          </input-checkbox>
+        </div>
       </div>
 
       <div class="field is-grouped">

commit 5d21bdb238c8e307cd84bd386ecb59375926ea21

Fixes #2085 : display GPS coordinate input
... below all editor maps (waypoint, outing, topo, user...)
(regression from 13d082d)

src/views/wiki/edition/utils/MapInputRow.vue
@@ -28,13 +28,13 @@
           @move="$emit('move', arguments[0])"
         />
       </div>
-      <div class="column is-6 is-hidden">
+      <div class="column is-6">
         <div class="field">
           <label class="label">{{ $gettext('Longitude') }}</label>
           <input-simple type="number" postfix="°E" v-model="longitude" @input="setGeometryPoint" />
         </div>
       </div>
-      <div class="column is-6 is-hidden">
+      <div class="column is-6">
         <div class="field">
           <label class="label">{{ $gettext('Latitude') }}</label>
           <input-simple type="number" postfix="°N" v-model="latitude" @input="setGeometryPoint" />