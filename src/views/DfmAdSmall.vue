36351f7120ae3670d8ca65688359f1edb501fa22
flob38 <florence.berbey@gmail.com>
2024-06-07 20:29:37 +0200

    DFM ad on home

---
+++ src/views/DfmAdSmall.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="camptocamp_htd" class="ams-ad" />
+</template>
+
+<script>
+export default {};
+
+var s = document.createElement('script');
+s.src = 'https://static.admysports.com/hb/site/camptocamp/hb_camptocamp.js';
+document.getElementsByTagName('head')[0].appendChild(s);
+</script>
