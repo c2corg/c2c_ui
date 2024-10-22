2798b61a467610d36c67deb372725f727ca9ba15
Vincent De Oliveira <vincent@iamvdo.me>
2024-10-22 14:00:21 +0200

    feat(yeti): Remove layer button from map

--- src/components/yeti/YetiMap.vue
+++ src/components/yeti/YetiMap.vue
@@ -6,15 +6,6 @@
       <map-layers />
       <yeti-layers />
       <overlays-layers />
-      <div
-        ref="layerSwitcherButton"
-        class="ol-control ol-control-layer-switcher-button"
-        :title="$gettext('Layers', 'Map controls')"
-      >
-        <button @click.stop="activeLayersTab">
-          <fa-icon icon="layer-group" width="100%" />
-        </button>
-      </div>
 
       <div ref="recenterOnControl" class="ol-control ol-control-recenter-on">
         <input type="text" :placeholder="$gettext('Recenter on...')" @input="searchRecenterPropositions" />
@@ -47,13 +38,6 @@
         <edit-mode-button />
       </div>
     </div>
-
-    <toast ref="toast-layer">
-      <template #title>
-        <span v-translate>Layers have moved</span>
-      </template>
-      <span v-translate>Now on the left, on the “Layers” tab</span> <fa-icon icon="layer-group" />
-    </toast>
   </div>
 </template>
 
@@ -63,7 +47,6 @@ import OverlaysLayers from './map-layers/OverlaysLayers.vue';
 import YetiLayers from './map-layers/YetiLayers.vue';
 
 import EditModeButton from '@/components/yeti/EditModeButton';
-import Toast from '@/components/yeti/Toast';
 import YetiMapLegend from '@/components/yeti/YetiMapLegend';
 import Yetix from '@/components/yeti/Yetix';
 import photon from '@/js/apis/photon';
@@ -80,7 +63,6 @@ export default {
     EditModeButton,
     MapLayers,
     OverlaysLayers,
-    Toast,
     YetiLayers,
     YetiMapLegend,
   },
@@ -135,7 +117,6 @@ export default {
     // add specific controls
     let controls = [
       new ol.control.FullScreen({ source: this.$el, tipLabel: this.$gettext('Toggle full-screen', 'Map Controls') }),
-      new ol.control.Control({ element: this.$refs.layerSwitcherButton }),
       new ol.control.Control({ element: this.$refs.editMode }),
       new ol.control.Control({ element: this.$refs.recenterOnControl }),
       new ol.control.Control({ element: this.$refs.recenterOnPropositions }),
@@ -218,10 +199,6 @@ export default {
     },
     onMapPointerMove(evt) {
       Yetix.$emit('map-pointermove', evt);
-    },
-    activeLayersTab() {
-      this.$refs['toast-layer'].toast();
-      Yetix.setActiveTab(0);
     },
     activateCenterOnGeolocation() {
       this.geolocation.setTracking(true);
