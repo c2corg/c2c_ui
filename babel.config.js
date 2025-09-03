a5a80d4f7f0e27f75a56a963147251b095f3a2f3
Bruno Besson <bruno.besson@viseo.com>
2025-09-03 13:59:54 +0200

    chore: obsolete plugins

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,3 @@
 module.exports = {
   presets: [['@vue/cli-plugin-babel/preset', { targets: { esmodules: true }, polyfills: [] }]],
-  /*
-   * Looks like since we updated browserslist + caniuse database to support only recent browsers, we need to manually
-   * include plugins.
-   * See https://github.com/vuejs/vue-loader/issues/1697#issuecomment-780455307
-   */
-  plugins: ['@babel/plugin-proposal-nullish-coalescing-operator', '@babel/plugin-proposal-optional-chaining'],
 };
